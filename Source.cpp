c3f4d31475c5bd2947214031bb7cd7584c0ce33e
mitra-31 <pavan.pushyamitra99@gmail.com>
2021-07-09 18:34:51 +0530

    Added new Sorting Algo

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,6 @@
 #include <Windows.h>
 #include <gl/glut.h>
 #include <bits/stdc++.h>
-#include "algos.h"
 
 
 
@@ -10,29 +9,56 @@
 const int N = 380;
 int vec[N], compares, exchanges;
 std::string sortName;
+char sort;
 
 
 
 void initGL();
 void display();
 void visual(int* arr);
-void visual(int* arr, int compares, int exchanges);
+void visual(int* arr,std::string sortName);
 void drawBitmapString(float x, float y, std::string s);
 
 
 void initGL() {
-	glClearColor(23.0/255.0f, 23.0 / 255.0f, 23.0 / 255.0f, 0.0f);
+	glClearColor(68.0/255.0f, 68.0 / 255.0f, 68.0 / 255.0f, 0.0f);
 }
 
 
+#include "algos.h"
 void display() {
-	visual(vec,compares,exchanges);
-	int arr[N];
-	sortName = "selection sort";
-	compares = 0, exchanges = 0;
-	for (int i = 0; i < N; i++)
-		arr[i] = vec[i];
-	insertionSort sort(arr, N,compares,exchanges);
+
+	visual(vec);	
+}
+
+
+void keyboardEvent(unsigned char c, int x, int y)
+{
+	if (c == 49)
+	{
+		drawBitmapString(700, 750, "Array Lenght: " + std::to_string(*(&vec + 1) - vec));
+		visual(vec, "Insertion Sort");
+		int arr[N];
+		sortName = "Inserrtion Sort";
+		compares = 0, exchanges = 0;
+		for (int i = 0; i < N; i++)
+			arr[i] = vec[i];
+		insertionSort sort(arr, N);
+		Sleep(10);
+	}
+	else if (c == 50)
+	{
+
+		drawBitmapString(700, 750, "Array Lenght: " + std::to_string(*(&vec + 1) - vec));
+		visual(vec, "Selection Sort");
+		int arr[N];
+		sortName = "Selection Sort";
+		compares = 0, exchanges = 0;
+		for (int i = 0; i < N; i++)
+			arr[i] = vec[i];
+		selectionSort sort(arr, N);
+		Sleep(10);
+	}
 }
 
 
@@ -41,12 +67,12 @@ void display() {
 	visual overloaded with only arr and (arr,comapres,exchanges)
 */ 
 
-void visual(int* arr,int comapres, int exchanges) {
+void visual(int* arr,std::string sortName) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	drawBitmapString(700, 770, sortName);
-	drawBitmapString(700, 750, "Number of Compares:" + std::to_string(compares));
-	drawBitmapString(700, 730, "Number of Exchanges:" + std::to_string(exchanges));
-	glColor3f(1.0f, 0.0f, 0.0f);
+	drawBitmapString(700, 740, "Number of Compares:" + std::to_string(compares));
+	drawBitmapString(700, 720, "Number of Exchanges:" + std::to_string(exchanges));
+	glColor3f(237.0/255.0f, 237.0/255.0f, 237.0/255.0f);
 	for (int i = 0; i < N; i++) {
 		glRecti(4 * i, 100, 4 * i + 3, 100 + arr[i]);
 	}
@@ -57,8 +83,7 @@ void visual(int* arr) {
 glClear(GL_COLOR_BUFFER_BIT);
 drawBitmapString(500, 750, "WELCOME TO SORTING VISUALIZER");
 drawBitmapString(520, 730, "USAGE");
-drawBitmapString(540, 700, "Press 'A' to Start All Alogrithms");
-drawBitmapString(560, 700, "Press '1' to Shell Sort");
+drawBitmapString(560, 700, "Press '1' to Insertion Sort.   Press '2' to Selection Sort.  Press '3' to Shell Sort");
 glColor3f(237.0 / 255.0f, 237.0 / 255.0f, 237.0 / 255.0f);
 for (int i = 0; i < N; i++) {
 	glRecti(4 * i, 100, 4 * i + 3, 100 + arr[i]);
@@ -92,10 +117,11 @@ int main(int argc, char** argv)
 	}
 
 	glutInit(&argc, argv);
-	glutInitWindowSize(1500, 864);
+	glutInitWindowSize(1000, 1000);
 	glutCreateWindow("Sorting Visualization");// name of window
 	glutInitWindowPosition(0, 100); // your screen cordinate at which window will appear
 	glutDisplayFunc(display); // call back function
+	glutKeyboardFunc(keyboardEvent);
 	initGL(); // initializing GL
 	gluOrtho2D(0, 1536, 0, 864); // transforming clipping area to Viewport area
 	glutMainLoop(); // infinite processing loop
--- algos.h
+++ algos.h
@@ -3,14 +3,14 @@
 
 class insertionSort {
 public:
-    insertionSort(int* arr, int N, int compares, int exchanges);
-    void sort(int* arr, int N,int compares, int exchanges);
+    insertionSort(int* arr, int N);
+    void sort(int* arr, int N);
     void swap(int* arr, int i, int j);
 };
-insertionSort::insertionSort(int* arr, int N,int compares, int exchanges) {
-    sort(arr, N, compares, exchanges);
+insertionSort::insertionSort(int* arr, int N) {
+    sort(arr, N);
 }
-void insertionSort::sort(int* arr, int N,int compares, int exchanges) {
+void insertionSort::sort(int* arr, int N) {
     for (int i = 1; i < N; i++)
     {
         for (int j = i; j >= 1; j--)
@@ -20,7 +20,7 @@ void insertionSort::sort(int* arr, int N,int compares, int exchanges) {
             {
                 exchanges++;
                 swap(arr, j, j - 1);
-                visual(arr, compares, exchanges);
+                visual(arr,"Insertion Sort");
             }
             else
                 break;
@@ -35,3 +35,38 @@ void insertionSort::swap(int* arr, int i, int j)
 }
 
 
+
+
+
+
+class selectionSort {
+public:
+    selectionSort(int* arr, int N);
+    void sort(int* arr, int N);
+    void swap(int* arr, int i, int j);
+};
+selectionSort::selectionSort(int* arr, int N) {
+    sort(arr, N);
+}
+void selectionSort::sort(int* arr, int N)
+{
+    for (int i = 0; i < N - 1; i++)
+    {
+        int ind = i;
+        for (int j = i + 1; j < N; j++)
+        {
+            compares++;
+            if (arr[j] < arr[ind])
+                ind = j;
+        }
+        exchanges++;
+        swap(arr, i, ind);
+        visual(arr,"Selection Sort");
+    }
+}
+void selectionSort::swap(int* arr, int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
